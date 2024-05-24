0ecf4591881be80e26ab95a3f736e791fabe4a9b
dagregi <120724828+dagregi@users.noreply.github.com>
2024-05-24 17:26:48 +0300

    feat: Add api for users

---
+++ api/users/index.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Method: GET');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With');
+
+require "../functions.php";
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == "GET") {
+	$userList = getUsers();
+	echo $userList;
+} else {
+	$data = [
+		'status' => 405,
+		'message' => $requestMethod . ' Method Not Allowed',
+	];
+
+	header("HTTP/1.0 405 Method Not Allowed");
+	echo json_encode($data);
+}
+
+?>
